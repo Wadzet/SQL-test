812bf387b6e3d294778e54331f293db23e8ef916
Vadym <116355658+Wadzet@users.noreply.github.com>
2023-11-18 18:16:15 +0200

    Update sql test.sql

--- sql test.sql
+++ sql test.sql
@@ -1,6 +1,6 @@
 -- Task 1: Subqueries 
 -- Objective: Retrieve the customer names and their orders' total price where the total price is higher than the average total price of all orders.
-
+USE tysql;
 
 SELECT Customers.cust_name, Orders.order_num,
 	SUM(OrderItems.quantity * OrderItems.item_price) AS total_price
@@ -19,11 +19,19 @@ HAVING total_price > (
 
 -- Task 2: Using Joins and Aggregate Functions 
 -- Objective: Retrieve the employee names and the number of orders they've taken.
+USE Northwind;
+
+SELECT Employee.firstname, Employee.lastname, COUNT(SalesOrder.orderId) AS numberOfOrders
+FROM Employee
+LEFT JOIN SalesOrder ON Employee.employeeId = SalesOrder.employeeId
+GROUP BY Employee.employeeId
+ORDER BY numberOfOrders DESC;
+
 
 
 -- Task 3: Handling NULL Values and CASE Statements 
 -- Objective: Show the product name and quantity; if the quantity is NULL, display "Out of Stock".
-
+USE tysql;
 
 SELECT prod_name, 
        CASE
@@ -38,9 +46,24 @@ ON Products.prod_id = product_quantity.prod_id;
 
 -- Task 4: Using Common Table Expressions (CTE) 
 -- Objective: List the orders made by French customers and display their total price.
+USE Northwind;
+
+WITH FrenchOrders AS (
+  SELECT SalesOrder.orderId, SalesOrder.custId, SUM(OrderDetail.unitPrice * OrderDetail.quantity) AS totalOrderPrice
+  FROM SalesOrder
+  JOIN OrderDetail ON SalesOrder.orderId = OrderDetail.orderId
+  JOIN Customer ON SalesOrder.custId = Customer.custId
+  WHERE Customer.country = 'France'
+  GROUP BY SalesOrder.orderId, SalesOrder.custId
+)
+
+SELECT FrenchOrders.orderId, FrenchOrders.custId, FrenchOrders.totalOrderPrice
+FROM FrenchOrders;
+
 
 -- Task 5: Working with Date Functions 
 -- Objective: Retrieve orders made in the last 90 days along with their shipping dates.
+USE tysql;
 
 SELECT 
     order_num AS 'Order', order_date AS 'Shipping Date'
@@ -49,7 +72,7 @@ WHERE order_date >= (SElECT MAX(order_date) - INTERVAL 90 DAY FROM Orders);
 
 -- Task 6: Nested Queries and Aggregation 
 -- Objective: Find the customers who have made the most expensive single order.
-
+use tysql;
 
 SELECT Customers.cust_id, Customers.cust_name,
     MAX(OrderItems.item_price * OrderItems.quantity) AS max_order
@@ -62,17 +85,17 @@ LIMIT 1;
 
 -- Task 7: Advanced Joins 
 -- Objective: List products with their suppliers and categories.
+USE Northwind;
 
--- -
-SELECT Products.prod_id, Products.prod_name,
-    Products.prod_price, Vendors.vend_name AS supplier
-FROM Products
-INNER JOIN Vendors ON Products.vend_id = Vendors.vend_id;
+SELECT Product.productName, Supplier.companyName AS supplier, Category.categoryName AS category
+FROM Product
+INNER JOIN Supplier ON Product.supplierId = Supplier.supplierId
+INNER JOIN Category ON Product.categoryId = Category.categoryId;
+
 
 -- Task 8: Handling Duplicates and Unions 
 -- Objective: Combine and display a list of all products and all categories.
-
-
+USE tysql;
 
 SELECT prod_name AS name
 FROM Products
@@ -82,7 +105,7 @@ FROM Vendors;
 
 -- Task 9: Using EXISTS 
 -- Objective: Display orders that have associated order details.
-
+USE tysql;
 
 SELECT *
 FROM Orders
@@ -95,7 +118,7 @@ WHERE EXISTS (
 
 -- Task 10: Data Modification with Transactions Objective: 
 -- Insert a new customer and an order for that customer in a single transaction.
-
+USE tysql;
 
 START TRANSACTION;
 INSERT INTO Customers
@@ -103,4 +126,6 @@ VALUES('1000000006', 'Molokija', 'Lozovetska 28', 'Ternopil', 'UA', '46000', 'UA
 INSERT INTO Orders
 VALUES(20010, '2023-11-10', '1000000006');
 COMMIT;
+
+
 
